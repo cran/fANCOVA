42ebbe545f3fa88b8977a9f8bbd6756b04d2b5b1
Xiaofeng Wang <wangx6@ccf.org>
2020-11-13 15:40:02 +0000

    version 0.6-1

--- R/onload.R
+++
@@ -1,2 +0,0 @@
-.onLoad <- function(lib, pkg)
-   packageStartupMessage("fANCOVA 0.5-1 loaded")
---
+++ R/zzz.R
@@ -0,0 +1,3 @@
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage("fANCOVA 0.6-1 loaded")
+}
